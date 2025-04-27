04d401646151f188349081d65a18180176249530
Kenny <kkha@indiana.edu>
2025-04-27 12:14:56 -0400

    Initial commit: Client and foundation started

---
+++ Models/Product.cs
@@ -0,0 +1,11 @@
+namespace ECommerce.Client.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public string? Category { get; set; }
+    }
+}
